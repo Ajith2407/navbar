06ffd34f331bcf508a508ec50a494b5c32258506
Ajith Kumar .D <90658192+Ajith2407@users.noreply.github.com>
2023-08-13 20:20:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menudata=document.querySelector('.menu-line')
+const menulist =document.querySelector('nav ul')
+menudata.addEventListener('click',()=>{
+    menulist.classList.toggle('showmenu')
+
+})
+
